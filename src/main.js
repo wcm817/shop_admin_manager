03f7b2828b6d0529c55a9313f413cea4166487ac
wucaimei <wucaimei817@163.com>
2019-10-07 17:42:05 +0800

    路由拦截器的设置和全局路由守卫

--- src/main.js
+++ src/main.js
@@ -9,6 +9,21 @@ import router from '@/router/router.js' // 引入router配置
 import App from './App.vue'
 Vue.use(ElementUi) // 往vue中注入 Element-Ui
 
+// 添加全局路由守卫
+router.beforeEach((to, from, next) => {
+  // 1. 获取token
+  let mytoken = localStorage.getItem('icast_manager_37')
+  // 2.判断
+  if (mytoken || to.path === '/login') {
+    // 如果有token值，证明已经登录过，随便访问，那么就next(),如果访问的是login，就直接跳转到login组件
+    // 下一步
+    next()
+  } else {
+    // 重定向，如果没有登录过，访问除了login之外的路径都跳转回login
+    next({ path: '/login' })
+  }
+
+})
 
 Vue.config.productionTip = false
 new Vue({
--- src/utils/myaxios.js
+++ src/utils/myaxios.js
@@ -2,5 +2,22 @@ import axios from 'axios'
 // 设置基准路径
 axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
 
+// 添加请求拦截器，可以拦截每一个axios请求
+// 拦截器的本质并不是来发起请求的，而是对请求报文进行处理
+axios.interceptors.request.use((config) => {
+  // 在发送请求之前做些什么:获取token，设置请求头
+  // 1. 获取token
+  let mytoken = localStorage.getItem('icast_manager_37')
+  if (mytoken) {
+    //  2.设置请求头:config.headers是一个对象，里面就包含着当前的请求头的设置
+    config.headers['Authorization'] = mytoken
+  }
+  // 返回config
+  return config
+}, function (error) {
+  // 对请求错误做些什么
+  //   Promise.reject(error):中止本次请求
+  return Promise.reject(error)
+})
 // 导出axios
 export default axios
