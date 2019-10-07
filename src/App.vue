8c0303f073fc345d15b7cd2e55beec45c94253e3
wucaimei <wucaimei817@163.com>
2019-10-07 10:53:32 +0800

    项目初始化，elementui，less，初始化样式已配置

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-breadcrumb separator="/">
+  <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+  <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
+  <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+  <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+</el-breadcrumb>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
+import ElementUi from 'element-ui' // 引入elementui组件
+import 'element-ui/lib/theme-chalk/index.css' // 引入elementui全局样式
+import '@/styles/element.less'
+import '@/styles/index.less'
+
 import App from './App.vue'
+Vue.use(ElementUi) // 往vue中注入 Element-Ui
+
 
 Vue.config.productionTip = false
-
 new Vue({
   render: h => h(App)
 }).$mount('#app')
